6cb40f709b34646f905357c3390cb7a6af35b85d
michael.nygreen <michael@nygreen.net>
2018-06-17 15:58:21 +0200

    Added dummy world cup repository, which just returns a couple of teams.

---
+++ WorldCup.Data.Repository/IWorldCupRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WorldCup.UI.Models;
+
+namespace WorldCup.Data.Repository
+{
+    public interface IWorldCupRepository
+    {
+        Task<Team[]> GetTeamsAsync();
+    }
+}
---
+++ WorldCup.Data.Repository/WorldCupRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using WorldCup.UI.Models;
+
+namespace WorldCup.Data.Repository
+{
+    public class WorldCupRepository : IWorldCupRepository
+    {
+        public Task<Team[]> GetTeamsAsync()
+        {
+            return Task.FromResult(new[]
+            {
+                new Team
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Denmark",
+                    Group = "C",
+                    Players = new Player[0]
+                },
+                new Team
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "France",
+                    Group = "C",
+                    Players = new Player[0]
+                }
+            });
+        }
+    }
+}
