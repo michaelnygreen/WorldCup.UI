b1f31081fc8322ecd2b3edef21910776ccb0dc9e
michael.nygreen <michael@nygreen.net>
2018-08-26 17:43:46 +0200

    Added value converter that can convert group name to an icon.

---
+++ WorldCup.UI.Views/ValueConverter/GroupValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WorldCup.UI.Views.ValueConverter
+{
+    class GroupValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string groupUri = $"Images/Groups/{value.ToString().ToLower()}.png";
+            return new BitmapImage(new Uri(groupUri, UriKind.Relative));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
