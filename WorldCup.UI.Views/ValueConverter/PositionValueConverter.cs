8fa9f5168c995552f52d9c4001bc1b6603283014
michael.nygreen <michael@nygreen.net>
2018-08-26 17:20:34 +0200

    Added value converter that can convert a position enum value to an abbreviated position string.

---
+++ WorldCup.UI.Views/ValueConverter/PositionValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using WorldCup.UI.Models;
+
+namespace WorldCup.UI.Views.ValueConverter
+{
+    class PositionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((Position) value)
+            {
+                case Position.Goalkeeper:
+                    return "GK";
+
+                case Position.Defender:
+                    return "D";
+
+                case Position.Midfielder:
+                    return "M";
+
+                case Position.Forward:
+                    return "F";
+
+                default:
+                    return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
