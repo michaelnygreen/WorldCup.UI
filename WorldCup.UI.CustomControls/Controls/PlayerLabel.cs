4c05aab82875ce16bd540ca68febf0896ca2538a
michael.nygreen <michael@nygreen.net>
2018-10-21 15:31:56 +0200

    Added label class for players on the pitch.

---
+++ WorldCup.UI.CustomControls/Controls/PlayerLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WorldCup.UI.CustomControls.Controls
+{
+    public class PlayerLabel : Label
+    {
+        public static readonly DependencyProperty BackgroundImageProperty =
+            DependencyProperty.Register("BackgroundImage", typeof(ImageSource), typeof(PlayerLabel), new PropertyMetadata(null));
+
+        static PlayerLabel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlayerLabel), new FrameworkPropertyMetadata(typeof(PlayerLabel)));
+        }
+
+        public ImageSource BackgroundImage
+        {
+            get { return (ImageSource)GetValue(BackgroundImageProperty); }
+            set { SetValue(BackgroundImageProperty, value); }
+        }
+    }
+}
