99e2adce7ff423b4969a8eeeb68c409101943462
michael.nygreen <michael@nygreen.net>
2018-10-21 13:01:58 +0200

    Added custom pitch control.

---
+++ WorldCup.UI.CustomControls/Controls/PitchControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WorldCup.UI.CustomControls.Controls
+{
+    public class PitchControl : ContentControl
+    {
+        private static double RequiredAspectRatio = 130.0 / 100;
+
+        static PitchControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PitchControl), new FrameworkPropertyMetadata(typeof(PitchControl)));
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            if (constraint.Height != double.NaN && constraint.Width != double.NaN && constraint.Height > 0 && constraint.Width > 0)
+            {
+                return CalculateSize(constraint);
+            }
+
+            return base.MeasureOverride(constraint);
+        }
+
+        private Size CalculateSize(Size size)
+        {
+            double aspectRatio = size.Height / size.Width;
+            if (aspectRatio > RequiredAspectRatio)
+            {
+                return new Size(size.Width, size.Width * RequiredAspectRatio);
+            }
+            else if (aspectRatio < RequiredAspectRatio)
+            {
+                return new Size(size.Height / RequiredAspectRatio, size.Height);
+            }
+
+            return size;
+        }
+    }
+}
